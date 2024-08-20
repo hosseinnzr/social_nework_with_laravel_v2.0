2d3c687b250932b516c2e76477e677ae1aacf125
hossein nazari <nazari.hossein28@gmail.com>
2024-08-20 13:03:03 +0330

    don't show delete account in search bar
    don't show delete/deactive account in search bar

--- app/Livewire/SearchBar.php
+++ app/Livewire/SearchBar.php
@@ -15,7 +15,7 @@ class SearchBar extends Component
         $result = [];
 
         if(strlen($this->search) >= 2){
-            $result = User::where('user_name', 'like', '%'.$this->search.'%')->limit(6)->get();
+            $result = User::where('status', 'active')->where('user_name', 'like', '%'.$this->search.'%')->limit(6)->get();
         }
 
         return view('livewire.search-bar', [
