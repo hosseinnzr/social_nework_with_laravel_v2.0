c1a96b4d8e32aa6223f40eca543037c25daf3d0d
hossein nazari <nazari.hossein28@gmail.com>
2024-09-24 11:17:10 +0330

    update search in header with search in header
    update search in header with search in header

--- app/Livewire/SearchBar.php
+++ app/Livewire/SearchBar.php
@@ -2,6 +2,8 @@
 
 namespace App\Livewire;
 
+use App\Models\hashtag;
+use App\Models\Post;
 use App\Models\User;
 use Livewire\Component;
 
@@ -12,14 +14,24 @@ class SearchBar extends Component
 
     public function render()
     {
-        $result = [];
+        $user_result = [];
+        $hashtag_result = [];
 
-        if(strlen($this->search) >= 2){
-            $result = User::where('status', 'active')->where('user_name', 'like', '%'.$this->search.'%')->limit(6)->get();
+        if(str_contains($this->search, '#')){
+            $new_search = str_replace('#', '', $this->search);
+            
+            if(strlen($this->search) >= 3){
+                $hashtag_result = hashtag::where('number','>=', '1')->where('name', 'like', '%'.$new_search.'%')->get();
+            }
+        }else{
+            if(strlen($this->search) >= 2){
+                $user_result = User::where('status', 'active')->where('user_name', 'like', '%'.$this->search.'%')->limit(6)->get();
+            }
         }
 
         return view('livewire.search-bar', [
-            'users' => $result
+            'users' => $user_result,
+            'hashtags' => $hashtag_result
         ]);
     }
 }
--- resources/views/livewire/search-bar.blade.php
+++ resources/views/livewire/search-bar.blade.php
@@ -1,15 +1,14 @@
 <form class="rounded position-relative">
-    <input class="form-control bg-light" type="search" placeholder="Search..." aria-label="Search" wire:model.live="search">
+    <input class="form-control bg-light" type="search" placeholder="Search" aria-label="Search" wire:model.live="search">
         @isset($users)
-        <div class="dropdown-menu dropdown-menu-size-md p-0 shadow-lg border-0 show">
+        <div class="dropdown-menu dropdown-menu-size-md p-0 shadow-lg border-0 show mt-1">
             @foreach ($users as $user)
 
             <div class="col-sm-6 col-lg-12">
-                <div class="card" >
                 <!-- Card body START -->
-                <div style="padding: 14px 0px 5px 14px" class="card-body">
+                <div style="padding: 5px 0px 5px 14px" class="card-body">
                     <!-- Connection item START -->
-                    <div class="hstack gap-2 mb-3">
+                    <div class="hstack gap-2 mb-1">
                         <!-- Avatar -->
                         <div class="avatar">
                             <a href="{{ route('profile', ['user_name' => $user['user_name']]) }}"><img class="avatar-img rounded-circle" src="{{$user['profile_pic']}}" alt=""></a>
@@ -25,7 +24,42 @@
                     <!-- Connection item END -->
                 </div>
                 <!-- Card body END -->
+            </div>
+            <!-- Card follow START -->
+            @endforeach
+
+            @foreach ($hashtags as $hashtag)
+
+            <div class="col-sm-6 col-lg-12">
+                <!-- Card body START -->
+                <div style="padding: 0px 0px 0px 14px" class="card-body">
+
+                    <!-- Connection item START -->
+                    <div class="hstack gap-0">
+                        <!-- Avatar -->
+                        <div class="avatar mt-2">
+                            <a class="btn btn-secondary-soft rounded-circle icon-md ms-auto" href="/explore/?tag={{$hashtag['name']}}"><i class="bi bi-hash"></i></a>
+                        </div>
+
+                        <div class="container">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <!-- name -->
+                                    <a style="color: black" class="mb-0" href="/explore/?tag={{$hashtag['name']}}">{{$hashtag['name']}}</a>
+                                </div>
+                                
+                                <div>
+                                    <!-- number -->
+                                    <p style="color: black" class="mb-0">{{$hashtag['number']}}</p>
+                                </div>
+                                
+                            </div>
+                        </div>
+
+                    </div>
+                    <!-- Connection item END -->
                 </div>
+                <!-- Card body END -->
             </div>
             <!-- Card follow START -->
             @endforeach
